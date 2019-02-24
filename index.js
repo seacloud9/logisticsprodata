d0e07c58a31f22f7583e4aeb4132d87643f333b9
Brendon Smith <bsmith@akiliinteractive.com>
2019-02-23 23:30:19 -0800

    logisticsprodata
    updating mutations and queries

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require("express");
-const { ApolloServer, gql } = require('apollo-server-express');
+const {
+    ApolloServer,
+    gql
+} = require('apollo-server-express');
 const moment = require('moment');
+
 const Trucks = []
 const Jobs = []
+const typeDefs = gql `
 
-  // Type definitions define the "shape" of your data and specify
-  // which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-
-input JobInput {
-    name: String!
-    dateOfMove: String!
-    startTime: String!
-    estimatedTime: Int!
-  }
 
 input TruckInput {
     name: String!
@@ -53,115 +48,152 @@ type JobQue {
   # the schema allows the following query:
 type Query {
     JobQue: [JobQue]
-    Job: [Job]
-    Truck: [Truck]
+    Jobs: [Job]
+    Trucks: [Truck]
 }
 
   # this schema allows the following mutation:
 type Mutation {
-    addJobQue (
+    JobQue (
       dateKey: String
       jobID: String
       truckID: String
     ): JobQue
-    addJob (
-      name: String
-      dateOfMove: String
-      startTime: String
-      estimatedTime: Int
+    Job (
+      name: String!
+      dateOfMove: String!
+      startTime: String!
+      estimatedTime: Int!
     ): Job
-    addTruck (
-      truck:TruckInput
+    Truck (
+      name: String!
+      startTime: Int!
+      endTime: Int!
     ): Truck
 }
   `;
 
-  const checkTruck = function(truck, job, jobQueID){
-    const currentTruck = find(Truck, { id: truck })
-    const currentJob = find(Job, {id: job})
-    const currentJobQue = find(JobQue, { id: jobQueID })
-    const currentDayTruckUsage = find(JobQue, { id: jobQueID, truck: truck.id})
+const checkTruck = function(truck, job, jobQueID) {
+    const currentTruck = find(Truck, {
+        id: truck
+    })
+    const currentJob = find(Job, {
+        id: job
+    })
+    const currentJobQue = find(JobQue, {
+        id: jobQueID
+    })
+    const currentDayTruckUsage = find(JobQue, {
+        id: jobQueID,
+        truck: truck.id
+    })
     if (!currentTruck) {
-      throw new Error(`Couldn't find the current ${truckId}`);
+        throw new Error(`Couldn't find the current ${truckId}`);
     }
-    if(currentDayTruckUsage){
-      let allofTruckCurrenHours = 0
-      currentDayTruckUsage.map((value, index) => {
-        allofTruckCurrenHours+= value.estimatedTime
-      })
-      if(0 > (allofTruckCurrenHours - currentTruck.totalHours)){
-        throw new Error(`Current truck ${truck.name} is unavailable`);
-      }
+    if (currentDayTruckUsage) {
+        let allofTruckCurrenHours = 0
+        currentDayTruckUsage.map((value, index) => {
+            allofTruckCurrenHours += value.estimatedTime
+        })
+        if (0 > (allofTruckCurrenHours - currentTruck.totalHours)) {
+            throw new Error(`Current truck ${truck.name} is unavailable`);
+        }
     }
 
     return {
-      id: jobQueID,
-      jobName: currentJob.name,
-      estimatedTime: currentJob.estimatedTime,
-      startTime: currentJob.startTime,
-      truck: truck
+        id: jobQueID,
+        jobName: currentJob.name,
+        estimatedTime: currentJob.estimatedTime,
+        startTime: currentJob.startTime,
+        truck: truck
+    }
+}
+
+const getUniqueID = _ => require('crypto').randomBytes(10).toString('hex')
+
+
+const resolvers = {
+    Query: {
+        JobQue: () => JobQue,
+        Jobs: () => Jobs,
+        Trucks: () => Trucks,
+    },
+    Mutation: {
+        JobQue: (_, {
+            dateKey,
+            jobID,
+            truckID
+        }) => {
+            const jobQue = find(JobQue, {
+                id: dateKey
+            });
+            if (!jobQue) {
+                return checkTruck(truckID, jobID, moment().format('MMMM Do YYYY, h:mm:ss a'))
+            } else {
+                return checkTruck(truckID, jobID, jobQue.id)
+            }
+        },
+        Job: (_, {
+            name,
+            dateOfMove,
+            startTime,
+            estimatedTime
+        }) => {
+            const _job = {
+                id: getUniqueID(),
+                name: name,
+                dateOfMove: dateOfMove,
+                startTime: startTime,
+                estimatedTime: estimatedTime
+            }
+            Jobs.push(_job)
+            return _job
+        },
+
+        Truck: (_, {
+            name,
+            startTime,
+            endTime
+        }) => {
+            // utilize military time for easy conversion on the backend
+            const _truck = {
+                id: getUniqueID(),
+                name: name,
+                startTime: startTime,
+                endTime: endTime,
+                totalHours: (endTime - startTime)
+            }
+            Trucks.push(_truck)
+            return _truck
+        }
     }
 }
 
 
-  const resolvers = {
-    Query: {
-      JobQue: () => JobQue,
-      Job: () => Jobs,
-      Truck: () => Trucks,
-    },
-    Mutation: {
-      addJobQue: (_, { dateKey, jobID, truckID }) => {
-        const jobQue = find(JobQue, { id: dateKey });
-        if (!jobQue) {
-          return checkTruck(truckID, jobID, moment().format('MMMM Do YYYY, h:mm:ss a'))
-        }else{
-          return checkTruck(truckID, jobID, jobQue.id)
-        }
-      },
-      addJob: (_, { name, dateOfMove, startTime, estimatedTime }) => {
-        const _job = {
-          id: require('crypto').randomBytes(10).toString('hex'),
-          name: name,
-          dateOfMove: dateOfMove,
-          startTime: startTime,
-          estimatedTime: estimatedTime
-        }
-        Jobs.push(_job)
-        return _job
-      },
-      addTruck: (_, { truck }) => {
-        return{
-          name: truck.name,
-          startTime: truck.startTime,
-          endTime: truck.endTime,
-          totalHours:truck.totalHours
-        }
-      },
-    }
-  }
-
-
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     engine: process.env.ENGINE_API_KEY && {
-      apiKey: process.env.ENGINE_API_KEY,
+        apiKey: process.env.ENGINE_API_KEY,
     }
-  });
+});
 
-  const app = express();
-  server.applyMiddleware({ app });
-  app.get("/", (req, res) => {
+const app = express();
+server.applyMiddleware({
+    app
+});
+app.get("/", (req, res) => {
     res.redirect("/graphql");
-  });
+});
 
-  // This `listen` method launches a web-server.  Existing apps
-  // can utilize middleware options, which we'll discuss later.
-  const port = 4000;
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+const port = 4000;
 
-  app.listen({ port }, () =>
+app.listen({
+        port
+    }, () =>
     console.log(
-      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+        `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
     )
-  )
+)
